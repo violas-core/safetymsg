a5832ee5955a2e2b5b3e5fb1a04d346cbee5298b
yinzhiqing <yinzhiqing@163.com>
2021-01-06 11:45:58 +0800

    added __init__.py __all__

---
+++ __init__.py
@@ -0,0 +1,6 @@
+import src.client
+smc_client = scr.client.safemsgclient
+smc_azure_names = smc_client.azure_names
+smc_key_source = smc_client.key_source
+
+__all__ = ["smc_client", "smc_azure_names", "smc_key_source"]
